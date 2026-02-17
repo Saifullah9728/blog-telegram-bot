90d105fa27808e3d8b2adece2d034fc7e64b8af4
Saifullah9728 <161334283+Saifullah9728@users.noreply.github.com>
2026-02-17 21:40:55 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,71 @@ TOKEN = os.getenv("BOT_TOKEN")
 user_data = {}
 
 BANNERS = {
-    "300": "<div>300x250 Banner Code</div>",
-    "728": "<div>728x90 Banner Code</div>",
-    "160": "<div>160x600 Banner Code</div>"
+    "Banner728x90": "<script>
+  atOptions = {
+    'key' : '62b66979469d06c7e271a2fb0866a58c',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/62b66979469d06c7e271a2fb0866a58c/invoke.js"></script>",
+    "Popunder": "<script src="https://pl25032984.effectivegatecpm.com/44/96/db/4496db75d868369cb78122c373a4dbce.js"></script>",
+    "Banner300x250": "<script>
+  atOptions = {
+    'key' : '16e15a2a4d22c6263410638c5e585ad3',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/16e15a2a4d22c6263410638c5e585ad3/invoke.js"></script>"
+    "Smartlink": "https://www.effectivegatecpm.com/wsbjyenm?key=cfcef4b39f4f531bed8bb02886c5794e"
+    "Banner160x300": "<script>
+  atOptions = {
+    'key' : '907e5e15ee1f4844972296bc26074971',
+    'format' : 'iframe',
+    'height' : 300,
+    'width' : 160,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/907e5e15ee1f4844972296bc26074971/invoke.js"></script>"
+    "Banner320x50": "<script>
+  atOptions = {
+    'key' : '5ab17c89e5e21c9f8df48bcc942be2b6',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/5ab17c89e5e21c9f8df48bcc942be2b6/invoke.js"></script>"
+    "SocialBar": "<script src="https://pl25044222.effectivegatecpm.com/e3/72/1d/e3721d3dcced2e475de6db8cc237209a.js"></script>"
+    "Banner 160x600": "<script>
+  atOptions = {
+    'key' : '0a60ce6d36d64c61fa35023d75939e84',
+    'format' : 'iframe',
+    'height' : 600,
+    'width' : 160,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/0a60ce6d36d64c61fa35023d75939e84/invoke.js"></script>"
+    "Banner468x60": "<script>
+  atOptions = {
+    'key' : '9d1f1820e3f7b781baf72cf6decfb08a',
+    'format' : 'iframe',
+    'height' : 60,
+    'width' : 468,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/9d1f1820e3f7b781baf72cf6decfb08a/invoke.js"></script>"
+    "NativeBanner": "<script async="async" data-cfasync="false" src="https://pl25033117.effectivegatecpm.com/932ef84e675d37d962fad2cbc6f62845/invoke.js"></script>
+<div id="container-932ef84e675d37d962fad2cbc6f62845"></div>"
 }
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
